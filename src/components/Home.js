4b377dbe636da1345d9f5aad65cc0efd715e6a59
root <root@DESKTOP-598GE48.localdomain>
2023-12-17 12:45:46 +0300

    Finished assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { username, city } from "../data/user"; // Adjust the path based on your file structure
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
+      <h1>      
+      {username} is a Web Developer from {city}
       </h1>
     </div>
   );
 }
+export default Home;
